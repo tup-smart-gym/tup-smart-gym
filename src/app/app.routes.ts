324880ce0653a4979ff24964f664901990009dc4
Elisa Ramos Farinho <elisaramosfarinho@Elisas-MacBook-Pro.local>
2026-05-29 08:45:37 -0300

    adjusting routes and html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-<<<<<<< 2-tp4-users
 import { UsersComponent } from './user/user';
-
-export const routes: Routes = [
-    {path: 'users', component: UsersComponent}
-=======
 import { SettingsComponent } from './components/settings/settings'; 
 
 export const routes: Routes = [
-    { path: 'configuracion', component: SettingsComponent }
->>>>>>> tp4
+    {path: 'users', component: UsersComponent},
+    {path: 'configuracion', component: SettingsComponent}
 ];
