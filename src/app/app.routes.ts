b2a50ba457f1cfd3215f86dc31bdb59027705fb0
Elisa Ramos Farinho <elisaramosfarinho@Elisas-MacBook-Pro.local>
2026-06-10 13:28:53 -0300

    session management and logging in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,22 @@
 import { Routes } from '@angular/router';
+import { authGuardFn } from '@auth0/auth0-angular';
 import { UsersComponent } from './user/user';
 import { SettingsComponent } from './components/settings/settings'; 
 import { LayoutComponent } from './main-layout/main-layout';
 import { LoginComponent } from './login/login';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { 
     path: 'main', 
     component: LayoutComponent,
+    canActivate: [authGuardFn],
     children: [
       { path: 'users', component: UsersComponent },
       { path: 'settings', component: SettingsComponent },
       { path: '', redirectTo: 'users', pathMatch: 'full' }
     ]
   },
-  { path: '**', redirectTo: '/login' }, 
+  { path: '**', component: LoginComponent }, 
 ];
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -22,25 +23,21 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 export class LoginComponent implements OnInit {
   public isLoading: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(public auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    if (this.isSessionActive()) {
-      this.router.navigate(['/main']);
-    }
+    this.auth.isAuthenticated$.subscribe(isAuthenticated => {
+      if(isAuthenticated){
+        this.router.navigate(['/main']);
+      }
+    })
   }
 
   public onLogin(): void {
     this.isLoading = true;
-
-    setTimeout(() => {
-      this.isLoading = false;
-      sessionStorage.setItem('session_active', 'true');
-      this.router.navigate(['/main']);
-    }, 2000);
+    this.auth.loginWithRedirect();
   }
 
-  // Cuando la imagen no carga, muestra el ícono de pesas como fallback
   public onImgError(event: Event): void {
     const img = event.target as HTMLImageElement;
     img.style.display = 'none';
@@ -54,7 +51,4 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  private isSessionActive(): boolean {
-    return sessionStorage.getItem('session_active') === 'true';
-  }
 }
