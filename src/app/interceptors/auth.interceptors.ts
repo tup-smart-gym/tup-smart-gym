507dd2a9502c7e000f7754f17f727b113afad150
Nicolas Licha <nicolaslicha30@gmail.com>
2026-08-09 21:20:32 -0300

    fix: agrego ngsw-bypass para que el service worker no intercepte las llamadas a la API

--- src/app/interceptors/auth.interceptors.ts
+++ src/app/interceptors/auth.interceptors.ts
@@ -1,25 +1,24 @@
-import { HttpInterceptorFn } from "@angular/common/http"; 
-import { inject } from "@angular/core";
-import { AuthService } from "@auth0/auth0-angular";
-import { switchMap } from "rxjs";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { switchMap } from 'rxjs';
 
 const isApiRequest = (url: string): boolean =>
-    url.includes('smartgymback-production-8639.up.railway.app') || 
-    url.includes('localhost:3000');
+  url.includes('smartgymback-production-8639.up.railway.app') || url.includes('localhost:3000');
 
-export const authInterceptor: HttpInterceptorFn = (req,next) => {
-    const auth = inject(AuthService);
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
 
-    if(!isApiRequest(req.url)){
-        return next(req);
-    }
+  if (!isApiRequest(req.url)) {
+    return next(req);
+  }
 
-    return auth.getAccessTokenSilently().pipe(
-        switchMap(token => {
-            const authReq = req.clone({
-                setHeaders: { Authorization: `Bearer ${token}`}
-            });
-            return next(authReq);
-        })
-    );
+  return auth.getAccessTokenSilently().pipe(
+    switchMap((token) => {
+      const authReq = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}`, 'ngsw-bypass': 'true' },
+      });
+      return next(authReq);
+    }),
+  );
 };
